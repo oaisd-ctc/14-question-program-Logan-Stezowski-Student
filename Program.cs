d3ce7c8194d5ebb80bf1968c35a89fc7fc8e4cd8
Logan-Stezowski-Student <lost2db@student.oaisd.org>
2024-10-16 17:21:45 +0000

    Added modules

--- Program.cs
+++ Program.cs
@@ -1,62 +1,114 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 public class Program
 {
+    public static string name;
+    public static string age;
+    public static string eyeColor;
+    public static string hairColor;
+    public static string shoeSize;
+    public static string color;
+    public static string movie;
+    public static string teacher;
+    public static string favClass;
+    public static string holiday;
+    public static string season;
+    public static string dreamJob;
+    public static int year;
+    public static int siblings;
+
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter name");
-        string name = Console.ReadLine();
-       
-
+        AskName();
+        AskAge();
+        AskEyeColor();
+        AskHairColor();
+        AskShoeSize();
+        AskFavoriteColor();
+        AskFavoriteTvShow();
+        AskFavoriteTeacher();
+        AskFavoriteClass();
+        AskFavoriteHoliday();
+        AskFavoriteSeason();
+        AskDreamJob();
+        CalulateAgeIn5Years(age);
+        NumberOfSiblings();
+        DisplaySummaryMessage();
+    }
+    public static void AskName() 
+    {
+         Console.WriteLine("Enter name");
+         name = Console.ReadLine();
+    }
+     public static void AskAge() 
+     {
         Console.WriteLine("Enter age");
-        int age = Convert.ToInt32(Console.ReadLine());
-        
-
-        Console.WriteLine("Enter eye color");
-        string eyeColor = Console.ReadLine();
-       
-
+         age = Console.ReadLine();
+    }
+     public static void AskEyeColor() 
+     {
+          Console.WriteLine("Enter eye color");
+         eyeColor = Console.ReadLine();
+    }
+     public static void AskHairColor() 
+     {
         Console.WriteLine("Enter hair color");
-        string hairColor = Console.ReadLine();
-       
-
+         hairColor = Console.ReadLine();
+    }
+     public static void AskShoeSize() 
+     {
         Console.WriteLine("Enter shoe size");
-        string shoeSize = Console.ReadLine();
-        
-
-        Console.WriteLine("Enter color");
-        string color = Console.ReadLine();
-       
-
+         shoeSize = Console.ReadLine();
+    }
+     public static void AskFavoriteColor() 
+     {
+          Console.WriteLine("Enter color");
+         color = Console.ReadLine();
+    }
+     public static void AskFavoriteTvShow() 
+     {
         Console.WriteLine("Enter tv show or movie");
-        string movie = Console.ReadLine();
-        
-
-        Console.WriteLine("Enter teacher");
-        string teacher = Console.ReadLine();
-        
-
-        Console.WriteLine("Enter class");
-        string favClass = Console.ReadLine();
-     
-
-        Console.WriteLine("Enter holiday");
-        string holiday = Console.ReadLine();
-        
-
+         movie = Console.ReadLine();
+    }
+     public static void AskFavoriteTeacher() 
+     {
+         Console.WriteLine("Enter teacher");
+         teacher = Console.ReadLine();
+    }
+     public static void AskFavoriteClass() 
+     {
+          Console.WriteLine("Enter class");
+         favClass = Console.ReadLine();
+    }
+     public static void AskFavoriteHoliday() 
+     {
+         Console.WriteLine("Enter holiday");
+         holiday = Console.ReadLine();
+    }
+     public static void AskFavoriteSeason() 
+     {
         Console.WriteLine("Enter season");
-        string season = Console.ReadLine();
-        
-
-        Console.WriteLine("Enter dream job");
-        string dreamJob = Console.ReadLine();
-        
-        int year = age + 5;
-
+         season = Console.ReadLine();
+    }
+     public static void AskDreamJob() 
+     {
+         Console.WriteLine("Enter dream job");
+         dreamJob = Console.ReadLine();
+    }
+     public static void CalulateAgeIn5Years(string age) 
+     {
+         year = Convert.ToInt32(age) + 5;
+    }
+     public static void NumberOfSiblings() 
+     {
         Console.WriteLine("Enter the number of siblings you have");
-         int siblings = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("My friend's name is " + name + ".  " + name + " is " + age + " years old.  " + name + "'s eye color is " + eyeColor + ".  " +
+         siblings = Convert.ToInt32(Console.ReadLine());
+    }
+     public static void DisplaySummaryMessage() 
+     {
+          Console.WriteLine("My friend's name is " + name + ".  " + name + " is " + age + " years old.  " + name + "'s eye color is " + eyeColor + ".  " +
          name + "'s hair color is " + hairColor + ".  " + name + "'s shoe size is " + shoeSize + ".  " + name + "'s favorite color is " + color + ".  " + name + "'s favorite tv show/movie is " + movie + ".  " + name + "'s favorite teacher is " + teacher + ".  " + 
          name + "'s favorite class is " + favClass + ".  " + name + "'s favorite holiday is " + holiday + ".  " + name + "'s favorite season is " + season + ".  " + name + "'s dream job is " + dreamJob + ".  " + name + " will be " + year + " in 5 years.  " 
          + name + " has " + siblings + " siblings.");
